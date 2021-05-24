326cfed2bef9b7b3e46ae23b0d38131a18a1d8bf
ieowlym <aika_7020@mail.ru>
2021-05-24 11:51:11 +0600

    cw-final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './cw-about/cw-about.component';
+import { ActionComponent } from './cw-action/cw-action.component';
+import { CCComponent } from './cw-cc/cw-cc.component';
+import { DonateComponent } from './cw-donate/cw-donate.component';
+import { IndexComponent } from './cw-index/cw-index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'action', component: ActionComponent },
+  { path: 'impact', component: CCComponent },
+  { path: 'donate', component: DonateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './cw-about/cw-about.component';
+import { ActionComponent } from './cw-action/cw-action.component';
+import { CCComponent } from './cw-cc/cw-cc.component';
+import { CountrySVGComponent } from './cw-country/cw-country.component';
+import { DonateComponent } from './cw-donate/cw-donate.component';
+import { IndexComponent } from './cw-index/cw-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    CountrySVGComponent,
+    ActionComponent,
+    CCComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
